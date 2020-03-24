435f4f60aaf40040f5a4ae5d6fa4da3cbaa7526f
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-03-24 17:50:07 -0300

    Backend desenvolvido (Aula 2)

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# Be the HERO!
+
+Aplicação desenvolvida em NodeJS, ReactJS e React Native (Semana OmniStack 11.0)
+
+Desenvolvido por Dominick Brasileiro
---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routes = express.Router()
+
+const SessionController = require('./controllers/SessionController')
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.store)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.store)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
+
+module.exports = routes
