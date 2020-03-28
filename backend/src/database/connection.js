a412d614927acb6f98f27ffaf465cadec017f9cc
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-03-27 21:04:53 -0300

    connection.js atualizado

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -4,5 +4,6 @@ const configuration = require('../../knexfile')
 const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development
 
 const connection = knex(config)
+connection.migrate.latest()
 
 module.exports = connection
