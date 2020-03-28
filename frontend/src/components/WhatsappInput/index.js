cf6984da9e39248b0b3cac7c85fcae83d9aea2af
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-03-27 23:07:45 -0300

    WhatsappInput refatorado

--- frontend/src/components/WhatsappInput/index.js
+++ frontend/src/components/WhatsappInput/index.js
@@ -3,14 +3,11 @@ import React, { useState } from 'react'
 import formatPhone from '../../utils/formatPhone'
 
 function WhatsappInput({onValueChange}) {
-  const [whatsapp, setWhatsapp] = useState('')
   const [formatedNumber, setFormatedNumber] = useState('')
 
   function handleChange(event) {
     const number = event.target.value.replace(/[()-\s]/g, '')
-    setWhatsapp(number)
     onValueChange(number)
-
     setFormatedNumber(formatPhone(number))
   }
 
