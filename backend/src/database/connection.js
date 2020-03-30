39d5df8b81479853cd99bc1c45231c356316d692
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-03-30 14:49:56 -0300

    connection.migrate removido

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -4,6 +4,5 @@ const configuration = require('../../knexfile')
 const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development
 
 const connection = knex(config)
-connection.migrate.latest()
 
 module.exports = connection
